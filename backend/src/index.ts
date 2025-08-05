a073291808f5dd74260de886ec277654d8acf0ed
Victor Oluwayemi <oluwayemivictor15@gmail.com>
2025-08-05 09:52:15 +0100

    feat: configure cors on backend server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,9 +2,27 @@ import express from 'express'
 import { connectToDatabase } from './config/db-config';
 import authRoutes from './routes/auth-routes'
 import userRoutes from './routes/user-routes';
+import cors from 'cors';
+
 
 connectToDatabase()
 const app = express();
+
+const allowedOrigins = ['http://localhost:5173', 'https://certi-verify.vercel.app/'];
+
+
+const corsOptions = {
+  origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
+     if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json())
 app.get('/', (req, res) => {
@@ -12,6 +30,7 @@ app.get('/', (req, res) => {
 });
 app.use('/api/auth', authRoutes)
 app.use('/api/user', userRoutes);
+
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
 });
